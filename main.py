183ed5afdf3584d7084d94c84f5b0695729831d8
Edmarck Sosa Pineda <e.sosa@csu.fullerton.edu>
2024-10-09 18:10:53 -0700

    Pushing all to github for github activity. Currently debugging.

---
+++ main.py
@@ -0,0 +1,8 @@
+import mathematics.numbers.series
+
+def main():
+    list = [1, 2, 3, 4, 5]
+    print(mathematics.numbers.series.sum(list=list))
+
+if __name__ == "__main__":
+    main()
---
+++ mathematics/numbers/series.py
@@ -0,0 +1,6 @@
+def sum(list):
+    return sum(list)
+
+def average(list):
+    return (sum(list) // len(list))
+
